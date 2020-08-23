a4a040a2a4693a30e6a919ee6c62e24a1852ee7c
Jc Dior <jessicaclairdior@gmail.com>
2020-08-23 21:05:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,7 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    return "HAPPY NEW YEAR!"
+  elsif number = 0
+      return "HAPPY NEW YEAR!"
   end
 end
